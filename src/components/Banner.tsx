2a8c3ea960c4f39edc02e8a9eaaea8de0a2c41a9
codenamemoe <newmoeali@gmail.com>
2024-03-18 14:41:35 +0000

    implement integration of contact form component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,20 @@
 import analyzeIcon from '@/app/assets/analyze-icon.svg'
 import background from '@/app/assets/background.svg'
 import Image from 'next/image'
+import { useState } from 'react'
+import { ContactFormDialog } from './ContactFormDialog'
 
 function Banner() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  const handleOpenDialog = () => setIsDialogOpen(true)
+
+  const handleCloseDialog = () => setIsDialogOpen(false)
+
+  const handleFormSubmitted = (message: any) => {
+    console.log(message)
+    handleCloseDialog()
+  }
   return (
     <div className='relative flex flex-col p-6 bg-[#003E49] rounded-lg text-white max-w-xs font-sans '>
       <div className='absolute top-0 left-0 right-0 h-64 rounded-t-lg overflow-hidden'>
@@ -33,9 +45,17 @@ function Banner() {
           <li>Recommendations of next steps to take</li>
         </ul>
       </div>
-      <button className='relative z-10 mt-6 py-2 px-4 bg-white text-[#008096] rounded-lg w-full text-sm transition duration-200 ease-in-out hover:bg-[#E1F8FB]'>
+      <button
+        onClick={handleOpenDialog}
+        className='relative z-10 mt-6 py-2 px-4 bg-white text-[#008096] rounded-lg w-full text-sm transition duration-200 ease-in-out hover:bg-[#E1F8FB]'
+      >
         Get In Touch
       </button>
+      <ContactFormDialog
+        open={isDialogOpen}
+        onClose={handleCloseDialog}
+        onFormSubmitted={handleFormSubmitted}
+      />
     </div>
   )
 }
