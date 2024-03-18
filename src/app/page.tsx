f8a39d6612933468be13c835cea72b26400bd145
codenamemoe <newmoeali@gmail.com>
2024-03-18 17:25:24 +0000

    Refactor Banner component for reusability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import analyzeIcon from '@/app/assets/analyze-icon.svg'
+import background from '@/app/assets/background.svg'
 import Banner from '@/components/Banner'
 import styles from './page.module.css'
 
@@ -12,9 +14,23 @@ export default function Home() {
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
       </div>
-
       <div className={styles.placeholder}>
-        <Banner />
+        <Banner
+          backgroundSrc={background}
+          iconSrc={analyzeIcon}
+          preSubmitTitle='Expert Report'
+          preSubmitSubTitle='Next Step'
+          preSubmitDescription='Please get in touch if you would like an expert report for this site. Benefits include:'
+          preSubmitListItems={[
+            'Key insights from our expert team',
+            'An in-depth analysis of the site',
+            'Recommendations of next steps to take',
+          ]}
+          preSubmitButtonLabel='Get In Touch'
+          postSubmitTitle='Request Received'
+          postSubmitDescription='We received your messge. Please feel free to get in touch again if you would like to include any further details or ask any other questions. We are eager to assist you.'
+          postSubmitButtonLabel='Send Another Request'
+        />
       </div>
     </main>
   )
--- src/components/NewBanner.tsx
+++ src/components/NewBanner.tsx
@@ -1,36 +1,15 @@
+import analyzeIcon from '@/app/assets/analyze-icon.svg'
+import background from '@/app/assets/background.svg'
 import Image from 'next/image'
 import { useState } from 'react'
 import { ContactFormDialog } from './ContactFormDialog'
 
-interface BannerProps {
-  backgroundSrc: string
-  iconSrc: string
-  preSubmitTitle: string
-  preSubmitDescription: string
-  preSubmitListItems: string[]
-  preSubmitButtonLabel: string
-  postSubmitTitle: string
-  postSubmitDescription: string
-  postSubmitButtonLabel: string
-  preSubmitSubTitle: string
-}
-
-const Banner: React.FC<BannerProps> = ({
-  backgroundSrc,
-  iconSrc,
-  preSubmitTitle,
-  preSubmitSubTitle,
-  preSubmitDescription,
-  preSubmitListItems,
-  preSubmitButtonLabel,
-  postSubmitTitle,
-  postSubmitDescription,
-  postSubmitButtonLabel,
-}) => {
+function Banner() {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [formSubmitted, setFormSubmitted] = useState(false)
 
   const handleOpenDialog = () => setIsDialogOpen(true)
+
   const handleCloseDialog = () => setIsDialogOpen(false)
 
   const handleFormSubmitted = () => {
@@ -39,49 +18,56 @@ const Banner: React.FC<BannerProps> = ({
   }
 
   return (
-    <div className='relative flex flex-col p-6 bg-[#003E49] rounded-lg text-white max-w-xs font-sans'>
+    <div className='relative flex flex-col p-6 bg-[#003E49] rounded-lg text-white max-w-xs lg:max-w-7xl font-sans'>
       <div className='absolute top-0 left-0 right-0 h-64 rounded-t-lg overflow-hidden'>
         <Image
-          src={backgroundSrc}
+          src={background}
           alt='Card background'
-          layout='fifitll'
+          layout='fit'
           objectFit='cover'
         />
       </div>
       <div className='relative z-10'>
         <div className='flex pt-8'>
-          <Image src={iconSrc} width={64} height={64} alt='Banner icon' />
+          <Image src={analyzeIcon} width={64} height={64} alt='Analyze icon' />
         </div>
         {formSubmitted ? (
           <div className='pt-4'>
-            <h1 className='text-xl font-normal'>{postSubmitTitle}</h1>
-            <p className='mt-12'>{postSubmitDescription}</p>
+            <h2 className='text-xl font-normal'>Expert Report Requested</h2>
+            <p className='mt-12'>
+              We received your message. Please feel free to get in touch again
+              if you would like to include any further details or ask any other
+              questions. We are eager to assist you.
+            </p>
             <button
               onClick={() => setFormSubmitted(false)}
               className='mt-6 py-2 px-4 bg-white text-[#008096] rounded-lg w-full text-sm transition duration-200 ease-in-out hover:bg-[#E1F8FB]'
             >
-              {postSubmitButtonLabel}
+              Send Another Message
             </button>
           </div>
         ) : (
           <>
             <div className='flex flex-col gap-1'>
-              <h2>{preSubmitSubTitle}</h2>
-              <h1 className='font-normal text-[24px]'>{preSubmitTitle}</h1>
+              <p className='font-normal'>Next step</p>
+              <p className='text-2xl font-normal'>Expert Report</p>
             </div>
             <div className='mt-8 max-w-[300px]'>
-              <p className='text-sm text-[16px]'>{preSubmitDescription}</p>
+              <p className='text-sm text-[16px]'>
+                Please get in touch if you would like an expert report for this
+                site. Benefits include:
+              </p>
               <ul className='list-disc mt-4 text-[16px] font-normal pl-5'>
-                {preSubmitListItems.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
+                <li>Key insights from our expert team</li>
+                <li>An in-depth analysis of the site</li>
+                <li>Recommendations of next steps to take</li>
               </ul>
             </div>
             <button
               onClick={handleOpenDialog}
               className='mt-6 py-2 px-4 bg-white text-[#008096] rounded-lg w-full text-sm transition duration-200 ease-in-out hover:bg-[#E1F8FB]'
             >
-              {preSubmitButtonLabel}
+              Get In Touch
             </button>
           </>
         )}
